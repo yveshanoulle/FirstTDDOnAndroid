182f006319adb33eeead0eda52d84b2de34d9f82
yveshanoulle <mailing@hanoulle.be>
2015-07-05 11:25:35 +0200

    added first TDD example test with Joppe

--- core/src/main/java/be/hanoulle/android/firsttddonandroid/core/HelloWorld.java
+++ core/src/main/java/be/hanoulle/android/firsttddonandroid/core/HelloWorld.java
@@ -2,7 +2,9 @@ package be.hanoulle.android.firsttddonandroid.core;
 
 public class HelloWorld {
     public String message(){
-
         return "42";
+    }
+    public int sum(int x, int y) {
+        return x+y ;
     }
 }
--- core/src/test/java/HelloWorldTest.java
+++ core/src/test/java/HelloWorldTest.java
@@ -8,8 +8,14 @@ public class HelloWorldTest {
 public void testHelloWorld() throws Exception {
     assertEquals("42", new HelloWorld().message());
 }
-
-
+    @Test
+    public void testSum2And3( ) throws Exception {
+        assertEquals(5, new HelloWorld().sum(2,3));
+    }
+    @Test
+    public void testSum3And4( ) throws Exception {
+        assertEquals(7, new HelloWorld().sum(3,4));
+    }
 
 
 }
